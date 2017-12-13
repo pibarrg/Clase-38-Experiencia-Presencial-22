68638eb96013ea08d41be1c956b3eb42594b3b0e
Escribe tu nombre sin tildes, y letras como ø, ñ <patricio.ibarra.g@gmail.com>
2017-12-13 12:10:28 -0300

    fifth_commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/landing'
+
   resources :students #crea las 7 rutas rest para el recurso (en este caso Student)
   root 'students#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
