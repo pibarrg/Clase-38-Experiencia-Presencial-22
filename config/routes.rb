50139015301d1e7f4c7496f6a4a560ba1424ec7d
Escribe tu nombre sin tildes, y letras como ø, ñ <patricio.ibarra.g@gmail.com>
2017-12-13 10:47:06 -0300

    second_commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :students #crea las 7 rutas rest para el recurso (en este caso Student)
+  root 'students#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
